df83d341dfd590c851c717c21e75221c86b57fa4
Sandu Victor <vyctor661@gmail.com>
2019-07-31 16:39:39 +0300

    Added a nice easteregg in the footer and the copyright thingy

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+  let today = new Date()
+  let year = today.getFullYear()
+
+  $(".copyright").html("Copyright &copy; " + year + " All rights reserved.");
+
+  $(".copyright").hover(function(){
+      $(".copyright").html("Nah JK, but please don't copy my website. If you won't that means you are a good person. :P");
+  },
+    function(){
+      $(".copyright").html("Copyright &copy; " + year + " All rights reserved.");
+    }
+
+);
+
+
+});
