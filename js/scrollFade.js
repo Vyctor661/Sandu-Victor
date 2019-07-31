c521ea15044cc8a29e4860867f8d3c3b7727e88b
Sandu Victor <vyctor661@gmail.com>
2019-07-31 16:39:11 +0300

    added the animation for the navbar, similar with the profile image

--- js/scrollFade.js
+++ js/scrollFade.js
@@ -1,19 +1,27 @@
 // This is a function for a fading effect for the main title
-$(window).scroll(function(){
-    $(".fadeIn").css("opacity", 1 - $(window).scrollTop() / 150);
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+      $(".fadeIn").css("opacity", 1 - $(window).scrollTop() / 150);
+    });
+  $(window).scroll(function(){
+      var minWid;
+      minWid = 180 - $(window).scrollTop()*1.5;
+      if(minWid < 0){
+        minWid = 0;
+      }
+      var minPad;
+      minPad = 0 - $(window).scrollTop();
+      if(minPad < -240){
+        minPad = -240;
+      }
+      $(".profileImage").css("width", minWid + "px");
+      $(".profileImage").css("margin-top", minPad + "px");
+
   });
 $(window).scroll(function(){
-    var minWid;
-    minWid = 180 - $(window).scrollTop()*1.5;
-    if(minWid < 0){
-      minWid = 0;
-    }
-    var minPad;
-    minPad = 0 - $(window).scrollTop();
-    if(minPad < -240){
-      minPad = -240;
-    }
-    $(".profileImage").css("width", minWid + "px");
-    $(".profileImage").css("margin-top", minPad + "px");
+    $("nav").css("left", -40 - $(window).scrollTop()/3 );
+});
+
 
 });
