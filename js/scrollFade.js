93b6f87e06f0d36d6e1d3f531d7cf8f44505a036
Sandu Victor <vyctor661@gmail.com>
2019-08-02 15:01:04 +0300

    first pannel done

--- js/scrollFade.js
+++ js/scrollFade.js
@@ -1,6 +1,17 @@
 //File for all fading effects on the page
 $(document).ready(function(){
 
+  function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
   $(window).scroll(function(){
       $(".fadeIn").css("opacity", 1 - $(window).scrollTop()/200);
     });
@@ -21,7 +32,15 @@ $(document).ready(function(){
   });
 $(window).scroll(function(){
     $(".nav").css("opacity", 1-$(window).scrollTop()/400 );
-    $(".nav").css("top", 80 - $(window).scrollTop()/30 + "%" );
+    $(".navContainer").css("top", 80 - $(window).scrollTop()/30 + "%" );
 });
 
+
+///This is for content cards
+$(window).scroll(function(){
+  if(isScrolledIntoView(".fromLeft")){
+    $(".card-left").animate({marginLeft: "0%", opacity: 1}, 400);
+    $(".card-right").animate({marginLeft: "0%", opacity: 1}, 400);
+  }
+});
 });
